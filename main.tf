929a78fe3095a3014c8b804a0eb932186ef7bb14
Isaac Hailperin <isaac.hailperin@lcsystems.ch>
2021-04-27 13:07:53 +0200

    adding provider with assume_role

--- main.tf
+++ main.tf
@@ -3,8 +3,17 @@ terraform {
 }
 
 provider "aws" {
+    alias = "default"
     region = var.aws_region
     version = "=2.55.0"
+}
+
+provider "aws" {
+  alias = "route53_account"
+  region = var.aws_region
+  assume_role {
+    role_arn = "arn:aws:iam::022053680926:role/bettertogether"
+  }
 }
 
 data "aws_availability_zones" "available" {}
@@ -215,6 +224,9 @@ data "aws_route53_zone" "selected" {
 }
 
 resource "aws_route53_record" "bastionhost" {
+  providers = {
+    aws = aws.route53_account
+  }
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = lookup(aws_instance.bastionhost.*.tags[0], "Name")
   #name    = "bastionhost"
@@ -225,6 +237,9 @@ resource "aws_route53_record" "bastionhost" {
 
 
 resource "aws_route53_record" "elb" {
+  providers = {
+    aws = aws.route53_account
+  }
   zone_id = data.aws_route53_zone.selected.zone_id
 #  name    = "${var.name}.data.aws_route53_zone.selected.name"
   name    = var.name
