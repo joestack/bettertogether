a84fdf20882b78f9bd88cab2e0685043d132de70
joestack <joern@joern-online.de>
2021-04-28 17:43:15 +0200

    joern pub webnodes 5

--- main.tf
+++ main.tf
@@ -353,6 +353,17 @@ resource "aws_security_group" "nat" {
 resource "aws_security_group_rule" "nat-http" {
   security_group_id = aws_security_group.nat.id
   type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+
+
+resource "aws_security_group_rule" "nat-http" {
+  security_group_id = aws_security_group.nat.id
+  type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
